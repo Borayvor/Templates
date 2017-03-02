143b9b228c492a6e83bd0239a54477a971d91de3
boray <borayvor@gmail.com>
2017-03-02 12:28:02 +0200

    rename user service project and folder

---
+++ MyMvcProjectTemplate/Services/MyMvcProjectTemplate.Services.ApplicationUser/ApplicationUserProfileService.cs
@@ -0,0 +1,29 @@
+﻿namespace MyMvcProjectTemplate.Services.ApplicationUser
+{
+    using System.Linq;
+    using Common.Contracts;
+    using Contracts;
+    using Data.Common.Repositories;
+    using Data.Models;
+
+    public class ApplicationUserProfileService : IApplicationUserProfileService,
+        IBaseGetService<ApplicationUser, string>
+    {
+        protected readonly IDbRepository<ApplicationUser> users;
+
+        public ApplicationUserProfileService(IDbRepository<ApplicationUser> users)
+        {
+            this.users = users;
+        }
+
+        public IQueryable<ApplicationUser> GetAll()
+        {
+            return this.users.All();
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return this.users.GetById(id);
+        }
+    }
+}
---
+++ MyMvcProjectTemplate/Services/MyMvcProjectTemplate.Services.ApplicationUser/Contracts/IApplicationUserProfileService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMvcProjectTemplate.Services.ApplicationUser.Contracts
+{
+    using Common.Contracts;
+    using Data.Models;
+
+    public interface IApplicationUserProfileService : IBaseGetService<ApplicationUser, string>
+    {
+    }
+}
