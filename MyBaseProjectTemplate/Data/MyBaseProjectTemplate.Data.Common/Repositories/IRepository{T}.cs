1bdecc13df26d5432747825ea34d23e3597d4211
Borayvor <Boray@abv.bg>
2017-05-30 11:58:15 +0300

    Initiate project

---
+++ MyBaseProjectTemplate/Data/MyBaseProjectTemplate.Data.Common/Repositories/IRepository{T}.cs
@@ -0,0 +1,18 @@
+﻿namespace MyBaseProjectTemplate.Data.Common.Repositories
+{
+    using System.Collections.Generic;
+
+    public interface IRepository<T>
+        where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(object id);
+
+        void Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ MyBaseProjectTemplate/Data/MyBaseProjectTemplate.Data.Common/Repositories/Repository{T}.cs
@@ -0,0 +1,34 @@
+﻿namespace MyBaseProjectTemplate.Data.Common.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
